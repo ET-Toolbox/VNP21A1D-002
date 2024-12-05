3f26466432a9b58240eca97a530296699ea89fce
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2024-12-04 16:42:45 -0800

    searching and downloading with earthaccess

--- VNP21A1D_002/VNP21A1D_002.py
+++ VNP21A1D_002/VNP21A1D_002.py
@@ -2,3 +2,5 @@ from .constants import *
 from .concept_ID_from_DOI import *
 from .VNP21A1D_granule import *
 from .VNP21A1D_CMR_search_links import *
+from .VIIRS_CMR_login import *
+from .VIIRS_CMR_query import *
