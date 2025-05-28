72702c05a2f3be859c9b22a6d20268d25149fc61
Gregory Halverson <gregory.halverson@gmail.com>
2025-05-28 16:21:17 -0700

    include object oriented interface

--- VNP21A1D_002/retrieve_granule.py
+++ VNP21A1D_002/retrieve_granule.py
@@ -1,5 +1,6 @@
 from typing import Union
 from datetime import date
+import earthaccess
 
 import VIIRS_tiled_granules
 
@@ -10,22 +11,24 @@ class GranuleNotFoundError(Exception):
     pass
 
 def retrieve_granule(
+        remote_granule: earthaccess.search.DataGranule = None,
         date_UTC: Union[date, str] = None,
         tile: str = None,
         download_directory: str = ".",
         parent_directory: str = None,
         concept_ID: str = VNP21A1D_002_CONCEPT_ID) -> VNP21A1DGranule:
-    remote_granules = VIIRS_tiled_granules.search_granules(
-        concept_ID=concept_ID,
-        date_UTC=date_UTC,
-        tile=tile,
-        tile_size=1200
-    )
+    if remote_granule is None:
+        remote_granules = VIIRS_tiled_granules.search_granules(
+            concept_ID=concept_ID,
+            date_UTC=date_UTC,
+            tile=tile,
+            tile_size=1200
+        )
 
-    if len(remote_granules) == 0:
-        raise GranuleNotFoundError(f"no VNP21A1D.002 granules found at tile {tile} on date {date_UTC}")
+        if len(remote_granules) == 0:
+            raise GranuleNotFoundError(f"no VNP21A1D.002 granules found at tile {tile} on date {date_UTC}")
 
-    remote_granule = remote_granules[0]
+        remote_granule = remote_granules[0]
 
     granule = VIIRS_tiled_granules.retrieve_granule(
         remote_granule, 
